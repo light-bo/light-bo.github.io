c1afa88bdddd06c27a1abc696e17125a7a10cdc1
light_bo <light_bo@126.com>
2022-04-11 21:37:09 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
-echo 'begin done'
+echo '>>> begin deploy'
 gitbook build
 
-echo '>> build success'
+echo '>>> build success'
 
 cp -r ./_book/ ../light-bo.github.io
 
-echo '>> copy files success'
+echo '>>> copy files success'
 
 git add --all
 git commit -m "update"
 git push
 
-echo '>> submit source code success'
+echo '>>> submit source code success'
 
 cd ../light-bo.github.io
 git add --all
 git commit -m "update"
 git push
 
-echo '>> deploy html success'
-echo '>> done'
+echo '>>> deploy html success'
+echo '>>> done'
